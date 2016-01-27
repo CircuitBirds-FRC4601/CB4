81f3c014c6b833d097167265d5b5b40d9a2b36b2
Christian Pruitt <christian9040@rocketmail.com>
2016-01-27 10:41:14 -0500

    Defines all the buttons on the xbox controller.
    /*Paul we _really_ miss you./*

---
+++ src/Gamepad.h
@@ -0,0 +1,24 @@
+#ifndef SRC_GAMEPAD_H_
+#define SRC_GAMEPAD_H_
+
+const int A = 1;
+const int B = 2;
+const int X = 3;
+const int Y = 4;
+const int LB = 5;
+const int RB = 6;
+const int Sel = 7;
+const int Sta = 8;
+const int lStick = 9;
+const int rStick = 10;
+
+const int lXA = 0;
+const int lYA = 1;
+const int lTA = 2;
+const int rTA = 3;
+const int rXA = 4;
+const int rYA = 5;
+
+float lat,rat;
+
+#endif /* SRC_GAMEPADX_H_ */
