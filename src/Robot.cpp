3d773ef8da8990f013e24c1093ea2d414d3b25bc
MIchael J. Crescimanno <dcphtn@gmail.com>
2016-02-05 22:26:38 -0500

    Pneumatic tested, 1/5/2016 code.
    Thanks Again to Elijah for writing the pneumatics code and the key mappings for both motions!

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,22 +1,25 @@
 #include "WPILib.h"
 #include "Gamepad.h"
-#include "CameraServer.h"
-
+#include "DoubleSolenoid.h"
   class Robot: public IterativeRobot
   {
+
  private:
  	LiveWindow *lw = LiveWindow::GetInstance();
   	SendableChooser *chooser;
   	bool nitroL, nitroR;  //DrC, for speed boost in tank drive
-  	bool pickup_pickup,shooter_spinup; //DrC
+  	bool pickup_pickup,shooter_spinup, piston_buttonu,piston_buttond,button_led; //DrC
   	const std::string autoNameDefault = "Default";
   	const std::string autoNameCustom = "My Auto";
  	double leftgo,rightgo,speed;  //DrC speed scales joysticks output to drive number
  	double ax, ay,az, bx,by, heading, boffsetx,boffsety, bscaley, bscalex, pd, pi=4.0*atan(1.0), pickup_kickballout, shooterWheel, pickupWheel, shooter_shoot; //FIX
   	std::string autoSelected;
 
-  	USBCamera *Camera =new USBCamera ("camera2",true);
-
+  	DoubleSolenoid *piston = new DoubleSolenoid(0,1);
+  	DoubleSolenoid *piston1 = new DoubleSolenoid(2,3);
+  	
+  	Compressor *howdy= new Compressor(0);//comnpressor does not like the term compress or compressor
+  	
   	Joystick *rightDrive = new Joystick(0,2,9);//DrC
   	Joystick *leftDrive  = new Joystick(1,2,9);//DrC
 	Joystick *gamePad = new Joystick(2,6,9);//DrC
@@ -39,8 +42,10 @@
   	RobotDrive *robotDrive = new RobotDrive(fLeft, bLeft, fRight, bRight);
   	RobotDrive *pickupShooter = new RobotDrive(pickup, pickup, shooter, shooter); //***
  void AutonomousInit() {
+	 howdy->Enabled();
 		lwheel->Reset();
 		rwheel->Reset();
+		piston1->Set(DoubleSolenoid::Value::kOff);
  		if(autoSelected == autoNameCustom){
  			//Custom Auto goes here
  		}
@@ -67,23 +72,32 @@
  				bscaley = 1.0;
  				lwheel->Reset();
  				rwheel->Reset();
- 				Camera->StartCapture();
+ 				howdy->Enabled();
+ 				piston1->Set(DoubleSolenoid::Value::kOff);
  	}
 
   	void TeleopPeriodic()
   	{
-  		Camera->OpenCamera();
+
   		rightgo = rightDrive-> GetRawAxis(1);
  		leftgo  = leftDrive-> GetRawAxis(1);
  		nitroR   = rightDrive-> GetRawButton(3);
  		nitroL   = leftDrive-> GetRawButton(3);
+ 		button_led = gamePad->GetRawButton(1);
+
  		speed  = .6;
 		rightgo = -(speed+(1.0-speed)*(double)(nitroR))*rightgo;  //DrC for nitro drive
  		leftgo  = -(speed+(1.0-speed)*(double)(nitroL))*leftgo;   //DrC  ''
  		robotDrive->TankDrive(rightgo, leftgo);
+
+ 		//Strobey bit
+ 		if(button_led){
+
+ 		led1->Pulse(1);
+ 		}
  		// Pickupwheel section
  		pickup_kickballout = gamePad -> GetRawAxis(2);//DrC
- 		pickup_pickup = gamePad -> GetRawButton(5);//DrC
+ 		pickup_pickup = gamePad -> GetRawButton(5);//rC
  		lwheel->GetRaw();
  		rwheel->GetRaw();
  		if(abs(pickup_kickballout)>.1){ //DrC
@@ -95,8 +109,10 @@
  		else{
  			pickupWheel=0.0;
  		}
- 		//
+
  		// right bumper speed up the wheel, right trigger  to shoot.
+ 		piston_buttonu   = gamePad-> GetRawButton(3);
+ 		piston_buttond   = gamePad-> GetRawButton(4);
  		shooter_shoot = gamePad -> GetRawAxis(3);//DrC
  		shooter_spinup = gamePad -> GetRawButton(6);
  		if(abs(shooter_shoot)>.1){ //DrC
@@ -105,6 +121,15 @@
  		 else {//DrC reset it so can have three modes, forward, backwards and nothing!
  			shooterWheel = 0.0;
  		 }
+ 		if(piston_buttonu)
+ 		{
+ 			piston->Set(DoubleSolenoid::Value::kReverse);
+ 		}
+ 		if(piston_buttond)
+ 		{
+ 		 			piston->Set(DoubleSolenoid::Value::kForward);
+ 		}
+
  		pickupShooter->TankDrive(pickupWheel,shooterWheel); //DrC here run the wheels!
  		//Sensor section Dr C
  		ax = accel-> GetX();//DrC   Sensor Section : get orientation of the robot WRT field co-ordinates.
